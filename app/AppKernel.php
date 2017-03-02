11d61fbf71fe262b2f1bf45aa8d6f7a10b1d1e06
WongBrice <brice_93200@hotmail.fr>
2017-03-02 13:01:20 +0100

    M2L v1.1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new M2L\PagesBundle\M2LPagesBundle(),
+            new M2L\UserBundle\M2LUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
@@ -23,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
--- src/M2L/PagesBundle/Controller/MainController.php
+++ src/M2L/PagesBundle/Controller/MainController.php
@@ -3,13 +3,9 @@
 namespace M2L\PagesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class MainController extends Controller
 {
-    /**
-     * @Route("/")
-     */
     public function indexAction()
     {
         return $this->render('M2LPagesBundle:Main:index.html.twig');
