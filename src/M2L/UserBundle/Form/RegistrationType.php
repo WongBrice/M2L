795aefbf100bf0c81e96739383d1793271dadf37
WongBrice <brice_93200@hotmail.fr>
2017-03-09 00:41:54 +0100

    Override FOSUserBundle setting 3

--- src/M2L/UserBundle/Form/RegistrationType.php
+++ src/M2L/UserBundle/Form/RegistrationType.php
@@ -4,19 +4,51 @@ namespace M2L\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-                ->add('first_name')
-                ->add('last_name')
-                ->add('address')
-                ->add('city')
-                ->add('postal_code')
-                ->add('phone')
+                ->add('first_name', 'text', array(
+                    'attr' => array(
+                        'placeholder' => 'Votre Prénom'
+                    )
+                ))
+                ->add('last_name', 'text', array(
+                    'attr' => array(
+                        'placeholder' => 'Votre Nom'
+                    )
+                ))
+                ->add('address', 'text', array(
+                    'attr' => array(
+                        'placeholder' => 'Votre Adresse'
+                    )
+                ))
+                ->add('city', 'text', array(
+                    'attr' => array(
+                        'placeholder' => 'Votre Commune'
+                    )
+                ))
+                ->add('postal_code', 'text', array(
+                    'attr' => array(
+                        'placeholder' => 'Votre Code Postal'
+                    )
+                ))
+                ->add('phone', 'text', array(
+                    'attr' => array(
+                        'placeholder' => 'Votre N° de Téléphone'
+                    )
+                ))
                 ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'M2L\UserBundle\Entity\User',
+        ));
     }
 
     public function getParent()
